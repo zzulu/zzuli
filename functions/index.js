a4de38697bde02bad58e7c6eb938966997c42867
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-06-03 19:10:04 +0900

    Add logging line & Change runtime options

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,28 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-// const cors = require('cors');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
 
 admin.initializeApp();
-const db = admin.firestore();
-const app = express();
-
-// Automatically allow cross-origin requests
-// app.use(cors({ origin: true }));
-
-// Add middleware to authenticate requests
-// app.use(myMiddleware);
+const db = admin.firestore()
+const app = express()
 
 app.get('/:shortened', async (req, res) => {
-  const doc = await db.collection('urls').doc(req.params.shortened).get();
+  const shortened = req.params.shortened
+  const doc = await db.collection('urls').doc(shortened).get()
   
   if (doc.exists) {
-    res.redirect(301, `${doc.data().origin}`);
+    console.info(`Redirect from: '${shortened}'`)
+    res.redirect(301, `${doc.data().origin}`)
   } else {
     // To Do: Make 404 Page
-    res.status(404).send('No such shortened URL!');
+    console.error(`No such shortened URL: '${shortened}'`)
+    res.status(404).send('No such shortened URL :(')
   }
 });
 
-// Expose Express API as a single Cloud Function:
-exports.redirect = functions.https.onRequest(app);
+const runtimeOpts = {
+  timeoutSeconds: 30,
+  memory: '128MB',
+}
+
+exports.redirect = functions.runWith(runtimeOpts).https.onRequest(app)

73aba5f3fc6076d6522115327dcbde5c738d6751
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-07-27 20:39:06 +0900

    Add 'return' to app callback func

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const express = require('express')
 
-admin.initializeApp();
+admin.initializeApp()
 const db = admin.firestore()
 const app = express()
 
@@ -12,16 +12,16 @@ app.get('/:shortened', async (req, res) => {
   
   if (doc.exists) {
     console.info(`Redirect from: '${shortened}'`)
-    res.redirect(301, `${doc.data().origin}`)
-  } else {
-    // To Do: Make 404 Page
-    console.error(`No such shortened URL: '${shortened}'`)
-    res.status(404).send('No such shortened URL :(')
+    return res.redirect(301, `${doc.data().origin}`)
   }
-});
+
+  // To Do: Make 404 Page
+  console.error(`No such shortened URL: '${shortened}'`)
+  return res.status(404).send('No such shortened URL :(')
+})
 
 const runtimeOpts = {
-  timeoutSeconds: 30,
+  timeoutSeconds: 10,
   memory: '128MB',
 }
 
